ba97d3b0b2a2bd3b26c24dd664ae0bd0869e09fa
Jonnathan-Landi <jonnathana.landi@ucuenca.edu.ec>
2025-01-31 01:59:55 -0500

    update tests

--- tests/testthat.R
+++ tests/testthat.R
@@ -6,6 +6,7 @@
 # * https://r-pkgs.org/testing-design.html#sec-tests-files-overview
 # * https://testthat.r-lib.org/articles/special-files.html
 
+library(data.table)
 library(testthat)
 library(RFplus)
 
--- tests/testthat/test-RFplus.R
+++ tests/testthat/test-RFplus.R
@@ -1,4 +1,4 @@
-test_that("RFplus works with included data", {
+test_that("RFplus works with different methods and included data", {
 
   Covariates = list(
     MSWEP = terra::rast(system.file("extdata/MSWEP.nc", package = "RFplus")),
@@ -9,11 +9,37 @@ test_that("RFplus works with included data", {
   BD_Insitu = data.table::fread(system.file("extdata/BD_Insitu.csv", package = "RFplus"))
   Cords_Insitu = data.table::fread(system.file("extdata/Cords_Insitu.csv", package = "RFplus"))
 
-  # Call function RFplus
-  result = RFplus(Covariates, BD_Insitu, Cords_Insitu, ntree = 2000, threshold = 0.1,
-                  n_round = 1, save_model = F, name_save = NULL, seed = 123)
+  # # Test with "QUANT" method
+  result_quant = RFplus(BD_Insitu, Cords_Insitu, Covariates, n_round = 1, wet.day = 0.1,
+                        ntree = 2000, seed = 123, method = "QUANT", ratio = 15,
+                        save_model = FALSE, name_save = NULL)
 
-  # Test
-  expect_s4_class(result, "SpatRaster")
-  expect_true(all(terra::values(result) >= 0))
+  expect_true(inherits(result_quant, "SpatRaster"))
+  expect_true(all(terra::values(result_quant, na.rm = T) >= 0))
+
+  # # Test with "RQUANT" method
+  # result_rquant = RFplus(BD_Insitu, Cords_Insitu, Covariates, n_round = 1, wet.day = 0.1,
+  #                        ntree = 2000, seed = 123, method = "RQUANT", ratio = 15,
+  #                        save_model = FALSE, name_save = NULL)
+  #
+  # expect_true(inherits(result_rquant, "SpatRaster"))
+  # expect_true(all(terra::values(result_rquant, na.rm = T) >= 0))
+  #
+  # # Test with "none" method
+  # result_none = RFplus(BD_Insitu, Cords_Insitu, Covariates, n_round = 1, wet.day = 0.1,
+  #                      ntree = 2000, seed = 123, method = "none", ratio = 15,
+  #                      save_model = FALSE, name_save = NULL)
+  #
+  # expect_true(inherits(result_none, "SpatRaster"))
+  # expect_true(all(terra::values(result_none, na.rm = T) >= 0))
+
+  # Additional test for the "wet.day" parameter with FALSE
+  # result_wet_day = RFplus(BD_Insitu, Cords_Insitu, Covariates, n_round = 1, wet.day = FALSE,
+  #                         ntree = 2000, seed = 123, method = "QUANT", ratio = 15,
+  #                         save_model = FALSE, name_save = NULL)
+  #
+  # expect_true(inherits(result_wet_day, "SpatRaster"))
+  # expect_true(all(terra::values(result_wet_day, na.rm = T) >= 0))
+
+
 })
