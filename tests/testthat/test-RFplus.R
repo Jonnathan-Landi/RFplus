f53006aadcdc4aa1771d0d7c9425ea8fbb84751f
Jonnathan-Landi <jonnathana.landi@ucuenca.edu.ec>
2025-02-01 19:35:33 -0500

    update data

--- tests/testthat/test-RFplus.R
+++ tests/testthat/test-RFplus.R
@@ -17,29 +17,12 @@ test_that("RFplus works with different methods and included data", {
   expect_true(inherits(result_quant, "SpatRaster"))
   expect_true(all(terra::values(result_quant, na.rm = T) >= 0))
 
-  # # Test with "RQUANT" method
-  # result_rquant = RFplus(BD_Insitu, Cords_Insitu, Covariates, n_round = 1, wet.day = 0.1,
-  #                        ntree = 2000, seed = 123, method = "RQUANT", ratio = 15,
-  #                        save_model = FALSE, name_save = NULL)
-  #
-  # expect_true(inherits(result_rquant, "SpatRaster"))
-  # expect_true(all(terra::values(result_rquant, na.rm = T) >= 0))
-  #
-  # # Test with "none" method
-  # result_none = RFplus(BD_Insitu, Cords_Insitu, Covariates, n_round = 1, wet.day = 0.1,
-  #                      ntree = 2000, seed = 123, method = "none", ratio = 15,
-  #                      save_model = FALSE, name_save = NULL)
-  #
-  # expect_true(inherits(result_none, "SpatRaster"))
-  # expect_true(all(terra::values(result_none, na.rm = T) >= 0))
+  # Test with "none" method
+  result_none = RFplus(BD_Insitu, Cords_Insitu, Covariates, n_round = 1, wet.day = 0.1,
+                       ntree = 2000, seed = 123, method = "none", ratio = 15,
+                       save_model = FALSE, name_save = NULL)
 
-  # Additional test for the "wet.day" parameter with FALSE
-  # result_wet_day = RFplus(BD_Insitu, Cords_Insitu, Covariates, n_round = 1, wet.day = FALSE,
-  #                         ntree = 2000, seed = 123, method = "QUANT", ratio = 15,
-  #                         save_model = FALSE, name_save = NULL)
-  #
-  # expect_true(inherits(result_wet_day, "SpatRaster"))
-  # expect_true(all(terra::values(result_wet_day, na.rm = T) >= 0))
-
+  expect_true(inherits(result_none, "SpatRaster"))
+  expect_true(all(terra::values(result_none, na.rm = T) >= 0))
 
 })
