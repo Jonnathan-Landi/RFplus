8ab8cf389c94009b372c85973b7cd93181ea2e4d
Jonnathan-Landi <jonnathana.landi@ucuenca.edu.ec>
2025-02-03 09:26:25 -0500

    update test

--- tests/testthat/test-RFplus.R
+++ tests/testthat/test-RFplus.R
@@ -16,13 +16,4 @@ test_that("RFplus works with different methods and included data", {
 
   expect_true(inherits(result_quant, "SpatRaster"))
   expect_true(all(terra::values(result_quant, na.rm = T) >= 0))
-
-  # Test with "none" method
-  result_none = RFplus(BD_Insitu, Cords_Insitu, Covariates, n_round = 1, wet.day = 0.1,
-                       ntree = 2000, seed = 123, method = "none", ratio = 15,
-                       save_model = FALSE, name_save = NULL)
-
-  expect_true(inherits(result_none, "SpatRaster"))
-  expect_true(all(terra::values(result_none, na.rm = T) >= 0))
-
 })
