830285189fdb4c21575d7985452d068d50c4a128
Jonnathan-Landi <jonnathana.landi@ucuenca.edu.ec>
2025-03-01 19:32:03 -0500

    update test

--- tests/testthat/test-RFplus.R
+++ tests/testthat/test-RFplus.R
@@ -10,9 +10,12 @@ test_that("RFplus works with different methods and included data", {
 
   # Test with "RQUANT" method
   result_quant = RFplus(BD_Insitu, Cords_Insitu, Covariates, n_round = 1, wet.day = 0.1,
-                        ntree = 2000, seed = 123, training = 0.8, Rain_threshold = 0.1,
+                        ntree = 2000, seed = 123, training = 0.8, Rain_threshold = list(no_rain = c(0, 1),
+                                                                                        light_rain = c(1, 5),
+                                                                                        moderate_rain = c(5, 20),
+                                                                                        heavy_rain = c(20, 40),
+                                                                                        violent_rain = c(40, 100)),
                         method = "RQUANT", ratio = 10, save_model = FALSE, name_save = NULL)
-
   expect_true(inherits(result_quant, "list"))
   expect_true(inherits(result_quant$Ensamble, "SpatRaster"))
   expect_true(all(terra::values(result_quant$Ensamble, na.rm = T) >= 0))
